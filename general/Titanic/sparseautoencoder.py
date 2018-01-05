474d20d8eea37ed9f5e9ff0452bfa7dccb5a5d04
Administrator <gongcheng_nankai@163.com>
2018-01-05 19:14:24 +0800

    update Titanic:  use sparse auto encoder and softmax to trian

--- general/Titanic/sparseautoencoder.py
+++ general/Titanic/sparseautoencoder.py
@@ -2,23 +2,45 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import predata
+import os
 
 file_path = './dataset/train.csv'
 savefile = True
 savepath = './data'
-_, _, tr, tr_l, _ = predata.predata(file_path, savefile, savename='train', savepath=savepath, ratio=0, predataflag=1)
+predataflag=1
+_, _, tr, tr_l, _ = predata.predata(file_path, savefile, savename='train', savepath=savepath, ratio=0, predataflag=predataflag)
+# 取训练集
+test_path = './dataset/test.csv'
+te, _, _, _, passengerIdlist = predata.predata(test_path, savefile, savename='test', savepath=savepath, ratio=1,
+                                           predataflag=predataflag)
+#对tr每一列做0均值单位方差
+eps=1e-4
+
+testnum=100
+te=tr[0:testnum]
+te_l=tr_l[0:testnum]
+tr=tr[testnum:]
+tr_l=tr_l[testnum:]
+tr=(tr-tr.mean(0))/(tr.std(0)+eps)
+te=(te-te.mean(0))/(te.std(0)+eps)
+te_lens=te_l.shape[0]
+te_hot=np.zeros([te_lens,2])
+te_hot[np.arange(0,te_lens),np.array(te_l,np.int32)]=1
+
+lens=tr_l.shape[0]
+one_hot=np.zeros([lens,2])
+one_hot[np.arange(0,lens),np.array(tr_l,np.int32)]=1
 lens = len(tr)
 # 对数据归一化，然后扔到稀疏编码器内
-input_nodes = 21  # 输入节点数
+input_nodes = 26  # 输入节点数
 hidden_size = 100  # 隐藏节点数
-output_nodes = input_nodes  # 输出节点数
+output_nodes = 2  # 输出节点数
 
 
 def sampleImage():
     batchsize = 100
     start = np.random.randint(0, lens - 100)
-    return tr[start:100 + start]
-
+    return tr[start:100 + start],one_hot[start:100 + start]
 
 # b=sampleImage()
 # 通过xvaier初始化第一层的权重值，xvaier初始化详见http://blog.csdn.net/shuzfan/article/details/51338178
@@ -27,21 +49,32 @@ def xvaier_init(input_size, output_size):
     high = -low
     return tf.random_uniform((input_size, output_size), low, high, dtype=tf.float32)
 
-
 # 计算代价函数，代价函数由三部分组成，均方差项，权重衰减项，以及稀疏因子项
-def computecost(w, b, x, w1, b1):
-    p = 0.01
+def computecost(w, b, x, w1, b1,keep_prob):
+    p = 0.9
     beta = 3
-    lamda = 0.00001
-
-    hidden_output = tf.sigmoid(tf.matmul(x, w) + b)
-    pj = tf.reduce_mean(hidden_output, 0)
+    lamda = 2
+    out=2
+    learnrate=1e-4
+    y_ = tf.placeholder(tf.float32, shape=[None, out], name='input_y')
+    hidden_output = tf.nn.relu(tf.nn.bias_add(tf.matmul(x, w),b))
+    fc2_drop = tf.nn.dropout(hidden_output, keep_prob)
+    pjj = tf.reduce_mean(hidden_output, 0)
+    pj=tf.clip_by_value(pjj, 1e-10,1-1e-10)
     sparse_cost = tf.reduce_sum(p * tf.log(p / pj) + (1 - p) * tf.log((1 - p) / (1 - pj)))
-    output = tf.sigmoid(tf.matmul(hidden_output, w1) + b1)
+    fcl2 = tf.nn.bias_add(tf.matmul(hidden_output, w1),b1)
+    # softmax多分类器
+    y_conv = tf.nn.softmax(fcl2)
     regular = lamda * (tf.reduce_sum(w * w) + tf.reduce_sum(w1 * w1)) / 2
-    cross_entropy = tf.reduce_mean(
-        tf.pow(output - x, 2)) / 2 + sparse_cost * beta + regular  # + regular+sparse_cost*beta
-    return cross_entropy, hidden_output, output
+
+    # cross_entropy = tf.reduce_mean(tf.pow(output - x, 2)) / 2 + sparse_cost * beta + regular  # + regular+sparse_cost*beta
+    cross_entropy = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y_conv, 1e-10, 1.0)))\
+                    + sparse_cost * beta + regular  # + regular+sparse_cost*beta
+    train_step = tf.train.AdamOptimizer(learnrate).minimize(cross_entropy)  # 调用优化器优化
+    pred=tf.argmax(y_conv, 1)
+    correct_prediction = tf.equal(pred, tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    return cross_entropy, hidden_output, y_conv,y_,train_step,accuracy,sparse_cost,regular,pred
 
 
 # 可视化自编码器：为了使隐藏单元得到最大激励（隐藏单元需要什么样的特征输入），将这些特征输入显示出来。
@@ -72,25 +105,42 @@ def show_image(w):
 def main():
     w = tf.Variable(xvaier_init(input_nodes, hidden_size))
     b = tf.Variable(tf.truncated_normal([hidden_size], 0.1))
-    x = tf.placeholder(tf.float32, shape=[None, input_nodes])
-    w1 = tf.Variable(tf.truncated_normal([hidden_size, input_nodes], -0.1, 0.1))
+    w1 = tf.Variable(tf.truncated_normal([hidden_size, output_nodes], -0.1, 0.1))
     b1 = tf.Variable(tf.truncated_normal([output_nodes], 0.1))
+    x = tf.placeholder(tf.float32, shape=[None, input_nodes])
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+    cost, hidden_output,y_conv,y_ ,train_step,accuracy,sparse_cost,regular,pred= computecost(w, b, x, w1, b1,keep_prob)
 
-    cost, hidden_output, output = computecost(w, b, x, w1, b1)
-    train_step = tf.train.AdamOptimizer().minimize(cost)
-    train_x = sampleImage()
+    train_x,train_l = sampleImage()
     sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
 
-    for i in range(100000):
-        _, hidden_output_, output_, cost_, w_ = sess.run([train_step, hidden_output, output, cost, w],
-                                                         feed_dict={x: train_x})
-        if i % 1000 == 0:
-            #             print(hidden_output_)
-            #             print(output_)
-            print('time (%d)'%(int(i/1000)),cost_)
-    np.save("weights1.npy", w_)
+    # 创建saver
+    saver = tf.train.Saver()
+    # 然后再session执行的时候，保存：
+    if os.path.exists('save/model.model.meta'):  # 判断模型是否存在
+        print('restore weightes form model!')
+        saver.restore(sess, tf.train.latest_checkpoint('save'))  # 存在就从模型中恢复变量
+    else:
+        # sess.run(tf.global_variables_initializer())
+        print('init weightes!')
+        #若不存在，则训练数据
+        sess.run(tf.global_variables_initializer())
+        for i in range(50000):
+            train_x, train_l = sampleImage()
+            if i % 1000 == 0:
+                #             print(hidden_output_)
+                #             print(output_)
+                cost_c,y_c,acc,sparse,reg=sess.run([cost,y_conv,accuracy,sparse_cost,regular], feed_dict={x: train_x, y_: train_l,keep_prob:0.5})
+                print('time (%d)'%(int(i/1000)),cost_c,' accuracy is %.4f sparse is %.4f regular is %.4f'%(acc,sparse,reg))
+                #保存参数
+                save_path = saver.save(sess, "save/model.model")
+            sess.run(train_step, feed_dict={x: train_x, y_: train_l})
+    # np.save("weights1.npy", w_)
     # show_image(w_)
+    #测试数据
+    acc=sess.run(accuracy, feed_dict={x: te,y_:te_hot,keep_prob:1.0})
+    print("test acc is %.4f"%(acc))
+    # predata.savepredictcsv(passengerIdlist, pred, 'softmax_test')
 
 if __name__ == '__main__':
     main()
