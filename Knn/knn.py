903da303f5d155aeec949656f21fe2cc5ea3f76c
gongcheng_nankai@163.com <adgjmptw13330393>
2018-01-16 20:42:01 +0800

    GMSmatrix: 实验快速获取块地址算法 bug1:为啥迭代和卷积统计出来的网格和不一样？ bug1答案：因为匹配点可能有多个，也就是说，可以一对多，也可以多对1，所以才会出现bug 解决方案： 近似解决：不管重复映射 精确解决：用邻域替换映射和偏置矩阵记录

---
+++ Knn/knn.py
@@ -0,0 +1,14 @@
+import numpy as np
+xr=100#x行数
+yr=1000#y的行数
+c=100#特征列数
+#模拟输入数据
+X=np.random.rand(xr,c)
+Y=np.random.rand(yr,c)
+def knn():
+    if(len(X[0])!=len(Y[0]))
+    Xdouble=np.diag(X.dot(X.T))
+    Ydouble=np.diag(Y.dot(Y.T))
+    return 0
+if __name__ == '__main__':
+    knn()
